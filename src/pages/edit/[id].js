d20df2da44b50e95c009dfeef5c483cd110495af
gustaf30 <gustavoferraz405@gmail.com>
2024-09-27 13:17:07 -0300

    Added color transition to all buttons index.ts (Light mode only)

--- src/pages/edit/[id].js
+++ src/pages/edit/[id].js
@@ -28,37 +28,48 @@ export default function EditTask() {
   };
 
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-2xl mb-4">Edit Task</h1>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <Input
-            value={task.title ?? ' '}
-            onChange={(e) => setTask({ ...task, title: e.target.value })}
-            placeholder="Task Title"
-            required
-          />
+    <div className="flex flex-col min-h-screen w-full p-6">
+      <header className="flex justify-between items-center mb-8">
+        <h1 className="text-3xl font-bold">
+          <a href={window.location.href.split('/edit')[0]}>
+            To do List
+          </a>
+        </h1>
+      </header>
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl mb-4 font-bold">Edit Task</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <Input
+              value={task.title ?? ' '}
+              onChange={(e) => setTask({ ...task, title: e.target.value })}
+              className="w-1/3 border hover:border-gray-500"
+              placeholder="Task Title"
+              required
+            />
         </div>
         <div className="mb-4">
           <Input
             value={task.description}
             onChange={(e) => setTask({ ...task, description: e.target.value })}
+            className="w-1/3 border hover:border-gray-500"
             placeholder="Description"
           />
         </div>
-        <div className="mb-4">
-          <Button
-            value={task.status}
-            onClick={(e) => setTask({ ...task, status: "0" })}>Pending</Button>
-          <Button
-            value={task.status}
-            onClick={(e) => setTask({ ...task, status: "1" })}>In progress</Button>
-          <Button
-            value={task.status}
-            onClick={(e) => setTask({ ...task, status: "2" })}>Completed</Button>
-        </div>
-        <Button type="submit">Save changes</Button>
-      </form>
+          <div className="mb-4">
+            <Button
+              value={task.status}
+              onClick={(e) => setTask({ ...task, status: "0" })}>Pending</Button>
+            <Button
+              value={task.status}
+              onClick={(e) => setTask({ ...task, status: "1" })}>In progress</Button>
+            <Button
+              value={task.status}
+              onClick={(e) => setTask({ ...task, status: "2" })}>Completed</Button>
+          </div>
+          <Button type="submit">Save changes</Button>
+        </form>
+      </div>
     </div>
   );
 }
